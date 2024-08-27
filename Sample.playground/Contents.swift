005ecc5562f0bae8935e4d673799b78fc7ceabfa
Sravani Kotha <kothasravani1834@gmail.com>
2024-08-27 10:21:19 -0500

    Initial commit

---
+++ Sample.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground, from Mukunda Sras"
+print(greeting)
+
+var n = 10
+var m = 20
+var sum =  n + m
+print(sum)
+print(n + 2)
+
+let k = 20
+print(k)
+
+var num = 10.45
+print(num)
